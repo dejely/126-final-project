af489c6c2e9890c699dcc3ebc937a61c03f40d9f
Yuan Birondo <ycbirondo@up.edu.ph>
2026-05-15 02:59:48 +0800

    feat: setup router redirects for all pages and initialized their functions

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import SeriesGame from "../pages/SeriesGame";
+import CharacterGame from "../pages/CharacterGame";
+import Achievements from "../pages/Achievements";
+import Leaderboard from "../pages/Leaderboard";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +14,18 @@ const router = createBrowserRouter([
     path: "/SeriesGame",
     element: <SeriesGame />,
   },
+  {
+    path: "/CharacterGame",
+    element: <CharacterGame />
+  },
+  {
+    path: "/Achievements",
+    element: <Achievements />
+  },
+  {
+    path: "/Leaderboard",
+    element: <Leaderboard />
+  }
 ]);
 
 export default router;
