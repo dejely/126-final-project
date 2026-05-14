0cad8c09b088fec6fadf00155d8585571ea6939b
Yuan Birondo <ycbirondo@up.edu.ph>
2026-05-15 03:06:29 +0800

    chore: added routing for invalid addresses/paths

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -4,6 +4,7 @@ import SeriesGame from "../pages/SeriesGame";
 import CharacterGame from "../pages/CharacterGame";
 import Achievements from "../pages/Achievements";
 import Leaderboard from "../pages/Leaderboard";
+import Error from "../pages/NotFound";
 
 const router = createBrowserRouter([
   {
@@ -25,6 +26,10 @@ const router = createBrowserRouter([
   {
     path: "/Leaderboard",
     element: <Leaderboard />
+  },
+  {
+    path: "*",
+    element: <Error/>
   }
 ]);
 
