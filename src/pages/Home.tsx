edf28f062ddf943cd46d877bed038e0629820b46
Renz Frederick <d3nzus@gmail.com>
2026-05-14 23:56:56 +0800

    feat: implement home with placeholder elements

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,32 @@ import Footer from '../components/layout/Footer';
 import Header from '../components/layout/Header';
 import Button from '../components/ui/Button';
 import Image from '../components/layout/Image';
+import Heading from '../components/layout/Heading';
 
 function Home() {
   return (
     <div className="homePage">
+      
+      <Image src='../public/home_background.png' alt="Home_Background" className="background" />
       <Header />
 
       <Image src='../public/aniguess_logo.png' alt="Aniguess_Logo" className="logo" />
+      <Image src='../public/help_button.png' alt="Help_Button" className="helpButton" />
+      <Image src='../public/settings_button.png' alt="Settings_Button" className="settingsButton" />
+      <Heading className='gamemodeHeading'>
+        Gamemodes
+      </Heading>
 
       <Button className='button' disabled={false}>
-        Generic Button Object
+        Series Rating
+      </Button>
+
+      <Button className='button' disabled={false}>
+        Series Faves
+      </Button>
+      
+      <Button className='button' disabled={false}>
+        Character Faves
       </Button>
 
       <Footer />
