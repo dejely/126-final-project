d11cb5cfa0b821cc25b97ee7a501eb1ba358dc39
Renz Frederick <d3nzus@gmail.com>
2026-05-26 12:55:10 +0800

    fix: error with title api import

--- src/features/anime/api/animeApi.ts
+++ src/features/anime/api/animeApi.ts
@@ -31,7 +31,7 @@ export async function getTop100() {
 
   return allData.map((item): AnimeData => ({ 
     id: item.mal_id,
-    title: item.title_english ?? item.title_japanese ?? item.title_synonyms?.[0] ?? 'Unknown Title',
+    title: item.title,
     image: item.images?.jpg?.image_url ?? '',
     rating: item.score,
     popularity: item.popularity,
